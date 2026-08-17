5c39351de1c53c755535eececcf1c07e2be3ce69
Discord-bot-123 <navm11433@gmail.com>
2026-08-18 01:47:59 +0530

    small web server so it can run on reder.com

--- src/index.js
+++ src/index.js
@@ -204,7 +204,6 @@ client.on('messageCreate', async message => {
 });
 
 // Keep-alive web server
-const express = require('express');
 const app = express();
 app.get('/', (req, res) => res.send('Bot is running'));
 app.listen(process.env.PORT || 3000);
